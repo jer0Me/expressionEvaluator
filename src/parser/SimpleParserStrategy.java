048d075f75be82c6917d38ab5ed27441202ab7d6
Jerome <Jerome@192.168.1.188>
2014-03-10 21:04:31 +0000

    created SimpleParserStrategy

---
+++ src/parser/SimpleParserStrategy.java
@@ -0,0 +1,15 @@
+package parser;
+
+public class SimpleParserStrategy extends ParserStrategy {
+
+    @Override
+    public void build(Token.Constant constant) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void build(Token.Symbol symbol) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
