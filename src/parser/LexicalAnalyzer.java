cd31609994d967d11514cc789a3724dafe957f5e
Jerome <Jerome@192.168.1.188>
2014-03-27 22:01:18 +0000

    corrected LexicalAnalyzer and parserTest

--- src/parser/LexicalAnalyzer.java
+++ src/parser/LexicalAnalyzer.java
@@ -1,27 +1,29 @@
 package parser;
 
-import parser.streamtokenizer.StreamTokenizerOperatorToken;
-import parser.streamtokenizer.StreamTokenizerTypeToken;
-import parser.streamtokenizer.StreamTokenizerNumberToken;
 import java.io.IOException;
 import java.io.StreamTokenizer;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
+import parser.streamtokenizer.StreamTokenizerNumberToken;
+import parser.streamtokenizer.StreamTokenizerOperatorToken;
+import parser.streamtokenizer.StreamTokenizerTypeToken;
 
 public class LexicalAnalyzer {
 
-    public Token[] analyze(String string) throws IOException {
+    public List<Token> analyze(String string) throws IOException {
         StreamTokenizer tokenizer = new StreamTokenizer(new StringReader(string));
         tokenizer.ordinaryChar('-');
+        tokenizer.ordinaryChar('/');
         return getListToken(tokenizer);
     }
 
-    private Token[] getListToken(StreamTokenizer tokenizer) throws IOException {
-        ArrayList<Token> tokens = new ArrayList<>();
+    private List<Token> getListToken(StreamTokenizer tokenizer) throws IOException {
+        List<Token> tokens = new ArrayList<>();
         while(tokenizer.nextToken() != StreamTokenizer.TT_EOF){
             tokens.add(getToken(tokenizer));
         }
-        return getArrayTokens(tokens);
+        return tokens;
     }
 
     private Token getToken(StreamTokenizer tokenizer) {
@@ -32,15 +34,6 @@ public class LexicalAnalyzer {
         return (tokenizer.ttype == StreamTokenizer.TT_NUMBER? 
                 new StreamTokenizerNumberToken(): new StreamTokenizerOperatorToken());
     }
-
-    private Token[] getArrayTokens(ArrayList<Token> tokens) {
-        Token[] listToken = new Token[tokens.size()];
-        for (int i = 0; i < tokens.size(); i++) {
-            listToken[i] = tokens.get(i);
-        }
-        return listToken;
-    }
-    
 }
 
 
--- test/parser/ParserTest.java
+++ test/parser/ParserTest.java
@@ -2,6 +2,7 @@ package parser;
 
 import evaluator.Constant;
 import java.io.IOException;
+import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -20,22 +21,33 @@ public class ParserTest {
     public void lexicalAnalyzerTest() throws IOException {
         LexicalAnalyzer analyzer = new LexicalAnalyzer();
         
-        Token[] listToken = analyzer.analyze("3+2");
+        List<Token> listToken = analyzer.analyze("3+2");
         
-        assertTrue(listToken[0].equals(Token.constant(3.0)));
-        assertTrue(listToken[1].equals(Token.symbol("+", 2, true)));
-        assertTrue(listToken[2].equals(Token.constant(2.0)));
+        assertTrue(listToken.get(0).equals(Token.constant(3.0)));
+        assertTrue(listToken.get(1).equals(Token.symbol("+", 2, true)));
+        assertTrue(listToken.get(2).equals(Token.constant(2.0)));
     }
     
     @Test
     public void lexicalAnalyzerTest2() throws IOException {
         LexicalAnalyzer analyzer = new LexicalAnalyzer();
         
-        Token[] listToken = analyzer.analyze("10.0-2");
+        List<Token> listToken = analyzer.analyze("10.0-2");
         
-        assertTrue(listToken[0].equals(Token.constant(10.0)));
-        assertTrue(listToken[1].equals(Token.symbol("-", 2, true)));
-        assertTrue(listToken[2].equals(Token.constant(2.0)));
+        assertTrue(listToken.get(0).equals(Token.constant(10.0)));
+        assertTrue(listToken.get(1).equals(Token.symbol("-", 2, true)));
+        assertTrue(listToken.get(2).equals(Token.constant(2.0)));
+    }
+    
+    @Test
+    public void lexicalAnalyzerTest3() throws IOException {
+        LexicalAnalyzer analyzer = new LexicalAnalyzer();
+        
+        List<Token> listToken = analyzer.analyze("10.0/2");
+        
+        assertTrue(listToken.get(0).equals(Token.constant(10.0)));
+        assertTrue(listToken.get(1).equals(Token.symbol("/", 2, true)));
+        assertTrue(listToken.get(2).equals(Token.constant(2.0)));
     }
     
     
